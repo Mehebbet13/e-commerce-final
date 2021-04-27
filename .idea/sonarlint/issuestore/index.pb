0ee2a26f1011a291610208897c69eba6812737fd
mahabbatcomardli <mahabbat.comardli@ibar.az>
2021-04-27 16:45:09 +0400

    created main page

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -7,3 +7,13 @@ h
 8app/src/main/java/com/example/e_commerce/MainActivity.kt,d/f/df2779f6009a84f5828051f66c07073afbc6f853
 H
 app/google-services.json,f/d/fd8d34b142d02ec743a8efefefd2c097961666e9
+o
+?app/src/main/java/com/example/e_commerce/ui/MainPageFragment.kt,6/e/6e7acbe0bfe3de26b0a5fd3645aa6c2e1dfda451
+k
+;app/src/main/java/com/example/e_commerce/ui/ShopFragment.kt,4/7/479707bc37536565bfe74bdad3b3201a1e3f9485
+n
+>app/src/main/java/com/example/e_commerce/ui/ProfileFragment.kt,b/4/b46dab9b2388f9ac52c6539981b5991ee3aa63f2
+j
+:app/src/main/java/com/example/e_commerce/ui/BagFragment.kt,3/6/361fc8d5f4a96f93ef8ba3542562132d0c87d0eb
+p
+@app/src/main/java/com/example/e_commerce/ui/FavoritesFragment.kt,e/f/efdeaec87d891f6d8a6fba32defd50941e42dbe8
--- app/src/main/java/com/example/e_commerce/MainActivity.kt
+++ app/src/main/java/com/example/e_commerce/MainActivity.kt
@@ -1,30 +1,68 @@
 package com.example.e_commerce
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupWithNavController
+import com.example.e_commerce.databinding.ActivityMainBinding
+import com.example.e_commerce.ui.*
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.gson.Gson
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var navController: NavController
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.navigation_bottom_menu, menu)
+        return true
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        navController = Navigation.findNavController(this, R.id.navhost_fragment)
+
+        bottomNavigationView.setupWithNavController(navController)
 
         val database = FirebaseDatabase.getInstance().reference
-        val getData = object : ValueEventListener{
+        val getData = object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 var i = 0
-                snapshot.children.forEach{
-                    it.child("Dresses/women").children.forEach{
-                        Log.i("mike",it.value.toString())
-                        Log.i("mike","$i")
+//                Log.e("mike type", snapshot.children)
+                snapshot.children.forEach {
+                    it.child("Dresses/women").children.forEach {
+//                        Log.i("mike", it.value.toString())
+//                        Log.i("mike", "$i")
                         i++
 
                     }
 
+                }
+                val gson = Gson()
+                val s1 = gson.toJson(snapshot.getValue())
+
+//                Log.e("string", s1)
+                val newJson = JSONObject(s1)
+                val panelKeys: Iterator<String> = newJson.keys()
+
+                while (panelKeys.hasNext()) {
+                    val panel: JSONObject =
+                        newJson.getJSONObject(panelKeys.next()) // get key from list
+//                    val id = panel.getString("women")
+//                    val number = panel.getString("number")
+
+//                    Log.e("string panel", panel["Dresses"].toString())
+//                    Log.e("string panelKeys", id)
                 }
 
             }
@@ -37,4 +75,5 @@ class MainActivity : AppCompatActivity() {
         database.addValueEventListener(getData)
         database.addListenerForSingleValueEvent(getData)
     }
+
 }
